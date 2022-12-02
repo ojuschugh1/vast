94c0685d1a97be00209cde550b179d4f0db235b1
Henrich Lauko <henrich.lauko@trailofbits.com>
2022-12-02 10:47:22 +0100

    hl:tocir: Update binary pattern naming.

--- lib/vast/Conversion/FromHL/ToCIR/ToCIR.cpp
+++ lib/vast/Conversion/FromHL/ToCIR/ToCIR.cpp
@@ -20,33 +20,6 @@ VAST_UNRELAX_WARNINGS
 
 namespace vast {
 
-    template< typename source, mlir::cir::BinOpKind kind >
-    struct arith_pattern : operation_conversion_pattern< source > {
-        using base = operation_conversion_pattern< source >;
-
-        using base::base;
-        using adaptor_t = typename source::Adaptor;
-
-        logical_result matchAndRewrite(source op, adaptor_t adaptor, conversion_rewriter &rewriter) const override {
-            rewriter.replaceOpWithNewOp< mlir::cir::BinOp >(op,
-                op.getType(), kind, adaptor.getLhs(), adaptor.getRhs()
-            );
-            return logical_result::success();
-        }
-
-        static void legalize(conversion_target &target) {
-            target.addLegalOp< mlir::cir::BinOp >();
-            target.addIllegalOp<
-                hl::AddIOp, hl::AddFOp,
-                hl::SubIOp, hl::SubFOp,
-                hl::MulIOp, hl::MulFOp,
-                hl::DivSOp, hl::DivUOp, hl::DivFOp,
-                hl::RemSOp, hl::RemUOp,  hl::RemUOp,
-                hl::BinXorOp, hl::BinOrOp, hl::BinAndOp,
-                hl::BinShlOp, hl::BinShrOp
-            >();
-        }
-    };
 
     //
     // function conversion
@@ -78,31 +51,58 @@ namespace vast {
     //
     // binary operations
     //
+    template< typename source, mlir::cir::BinOpKind kind >
+    struct binary_pattern : operation_conversion_pattern< source > {
+        using base = operation_conversion_pattern< source >;
+
+        using base::base;
+        using adaptor_t = typename source::Adaptor;
+
+        logical_result matchAndRewrite(source op, adaptor_t adaptor, conversion_rewriter &rewriter) const override {
+            rewriter.replaceOpWithNewOp< mlir::cir::BinOp >(op,
+                op.getType(), kind, adaptor.getLhs(), adaptor.getRhs()
+            );
+            return logical_result::success();
+        }
+
+        static void legalize(conversion_target &target) {
+            target.addLegalOp< mlir::cir::BinOp >();
+            target.addIllegalOp<
+                hl::AddIOp, hl::AddFOp,
+                hl::SubIOp, hl::SubFOp,
+                hl::MulIOp, hl::MulFOp,
+                hl::DivSOp, hl::DivUOp, hl::DivFOp,
+                hl::RemSOp, hl::RemUOp,  hl::RemUOp,
+                hl::BinXorOp, hl::BinOrOp, hl::BinAndOp,
+                hl::BinShlOp, hl::BinShrOp
+            >();
+        }
+    };
 
     using arithmetic_conversions = util::type_list<
-        arith_pattern< hl::AddIOp, mlir::cir::BinOpKind::Add >,
-        arith_pattern< hl::AddFOp, mlir::cir::BinOpKind::Add >,
-        arith_pattern< hl::SubIOp, mlir::cir::BinOpKind::Sub >,
-        arith_pattern< hl::SubFOp, mlir::cir::BinOpKind::Sub >,
-        arith_pattern< hl::MulIOp, mlir::cir::BinOpKind::Mul >,
-        arith_pattern< hl::MulFOp, mlir::cir::BinOpKind::Mul >,
-        arith_pattern< hl::DivSOp, mlir::cir::BinOpKind::Div >,
-        arith_pattern< hl::DivUOp, mlir::cir::BinOpKind::Div >,
-        arith_pattern< hl::DivFOp, mlir::cir::BinOpKind::Div >,
-        arith_pattern< hl::RemSOp, mlir::cir::BinOpKind::Rem >,
-        arith_pattern< hl::RemUOp, mlir::cir::BinOpKind::Rem >,
-        arith_pattern< hl::RemUOp, mlir::cir::BinOpKind::Rem >
+        binary_pattern< hl::AddIOp, mlir::cir::BinOpKind::Add >,
+        binary_pattern< hl::AddFOp, mlir::cir::BinOpKind::Add >,
+        binary_pattern< hl::SubIOp, mlir::cir::BinOpKind::Sub >,
+        binary_pattern< hl::SubFOp, mlir::cir::BinOpKind::Sub >,
+        binary_pattern< hl::MulIOp, mlir::cir::BinOpKind::Mul >,
+        binary_pattern< hl::MulFOp, mlir::cir::BinOpKind::Mul >,
+        binary_pattern< hl::DivSOp, mlir::cir::BinOpKind::Div >,
+        binary_pattern< hl::DivUOp, mlir::cir::BinOpKind::Div >,
+        binary_pattern< hl::DivFOp, mlir::cir::BinOpKind::Div >,
+        binary_pattern< hl::RemSOp, mlir::cir::BinOpKind::Rem >,
+        binary_pattern< hl::RemUOp, mlir::cir::BinOpKind::Rem >,
+        binary_pattern< hl::RemUOp, mlir::cir::BinOpKind::Rem >
     >;
 
     using binary_conversions = util::type_list<
-        arith_pattern< hl::BinXorOp, mlir::cir::BinOpKind::Xor >,
-        arith_pattern< hl::BinOrOp,  mlir::cir::BinOpKind::Or  >,
-        arith_pattern< hl::BinAndOp, mlir::cir::BinOpKind::And >
+        binary_pattern< hl::BinXorOp, mlir::cir::BinOpKind::Xor >,
+        binary_pattern< hl::BinOrOp,  mlir::cir::BinOpKind::Or  >,
+        binary_pattern< hl::BinAndOp, mlir::cir::BinOpKind::And >
     >;
 
     using shift_conversions = util::type_list<
-        arith_pattern< hl::BinShlOp, mlir::cir::BinOpKind::Shl >,
-        arith_pattern< hl::BinShrOp, mlir::cir::BinOpKind::Shr >
+        binary_pattern< hl::BinShlOp, mlir::cir::BinOpKind::Shl >,
+        binary_pattern< hl::BinShrOp, mlir::cir::BinOpKind::Shr >
     >;
 
     struct HLToCIRPass : ModuleConversionPassMixin< HLToCIRPass, HLToCIRBase > {
